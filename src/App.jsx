7b9c9b8cbcb367a5a9b045aac62f8dc368076a93
dcortesnet <dcortes.net@gmail.com>
2024-02-26 16:59:52 -0300

    Add style and state

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  let [selected, setSelected] = useState(null);
+
+  const handleSelected = (color) => {
+    setSelected(color);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="traffic-light">
+        <div
+          className={
+            "circle circle-red " + (selected === "red" ? "turn-on" : "")
+          }
+          onClick={() => handleSelected("red")}
+        ></div>
+        <div
+          className={
+            "circle circle-orange " + (selected === "orange" ? "turn-on" : "")
+          }
+          onClick={() => handleSelected("orange")}
+        ></div>
+        <div
+          className={
+            "circle circle-green " + (selected === "green" ? "turn-on" : "")
+          }
+          onClick={() => handleSelected("green")}
+        ></div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
